634d744a3429816d3da33f1b7d7a855bd98c2938
YuranShi <62267207+YuranShi@users.noreply.github.com>
2020-04-28 19:19:51 -0400

    sportscar

---
+++ SportsCar.java
@@ -0,0 +1,33 @@
+
+public class SportsCar extends RaceCar {
+
+	public SportsCar(int speed, int strength) { //car parameter
+		if(speed>45){
+			this.speed=45;
+		}
+		else if(speed<20){
+			this.speed=20;
+		}
+		else{
+			this.speed=speed;
+		}
+		if(strength>3){
+			strength=3;
+		}
+		else if(strength<1){
+			strength=1;
+		}
+		this.strength=strength;
+		this.lap=1;
+		this.unit=0;
+		this.last_unit=0;
+		this.last_damaged=false;
+		this.damaged=false;
+		this.ifpit=false;
+		this.departfrompit=false;
+	}
+	
+	public SportsCar() {
+		this(30,2);
+	}
+}
